aa982a53b445187ff4bd439386c63b2dbcdef2d7
jxmked <jamc015lol@gmail.com>
2022-08-30 07:07:11 +0800

    Update

--- bin/index.js
+++ bin/index.js
@@ -3,10 +3,10 @@
 const fs = require('fs');
 const path = require('path');
 const argParser = require('commander');
-const { exists, overwritePrint } = require('../lib/helpers.js');
+const { exists, overwritePrint, configs, mkdir, isFolder } = require('../lib/helpers.js');
 const { Start, getScanned } = require('../lib/getAllFiles.js');
-
-
+const logger = require('../lib/logger.js');
+const minify = require('../lib/minify.js');
 
 // Package Info
 const pkg = require('../package.json');
@@ -36,8 +36,44 @@ if(!exists(input)) {
 // Start Scanning
 process.stdout.write(`Scanning directory: ${global.input}\n`);
 Start(global.input);
-overwritePrint(`Scanned ${getScanned().length} files and folders.\n`);
-//StartLogging();
-//CreateMirror();
+overwritePrint(`Scanned ${getScanned().length} files and folders.`);
+console.log();
 
-//console.log(getScanned())
+const FOLDERS = [];
+const FILES = [];
+
+// Seperate Folder and Files
+getScanned().forEach((value) => {
+    if(isFolder(value)) {
+        FOLDERS.push(value);
+        
+        // Create Folder Structure
+        mkdir(value.replace(global.input, global.output));
+    } else {
+        FILES.push(value);
+    }
+})
+
+// Get Modifed Files
+const modified = logger.getModifiedFiles(FILES);
+
+if(modified.length > 0) {
+    console.log(`About ${modified.length} file(s) has been modified.\n`);
+    
+    // Start Minification
+    console.log("\nMinifying...");
+    minify.setInOut(global.input, global.output);
+    
+    // Start and get files that failed to take actions
+    const failed = minify.start(modified);
+    console.log("\n");
+    
+    // Save files that has been minified or copied
+    logger.saveLogs(FILES.filter((file) => {
+        return failed.indexOf(file) == -1;
+    }));
+    
+    overwritePrint('Minified!');
+} else {
+    console.log("Looks clean...");
+}
